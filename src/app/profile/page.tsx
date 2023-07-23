36621e89f55ff1108da3ed65c000f69d8800330b
Dhruv Maheshwari <mrdm026@gmail.com>
2023-07-24 03:10:49 +0530

    login and signup complete

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function ProfilePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile Page</p>
+    </div>
+  );
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,45 @@
 "use client";
 
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+
 export default function SignUpPage() {
-  return (
+  const router = useRouter();
+  const [user, setUser] = React.useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
+
+  const onSignUp = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Sign Up Success.", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.error("Sign Up Failed", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    }
+  }, [user]);
+
+  return !loading ? (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
         <div>
@@ -14,18 +52,30 @@ export default function SignUpPage() {
                 className="w-full px-6 py-3 text-sm font-medium text-white placeholder-white bg-transparent border-2 border-white focus:outline-none focus:bg-transparent"
                 type="text"
                 placeholder="Username"
+                id="username"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
               />
               <input
                 className="w-full px-6 py-3 mt-5 text-sm font-medium text-white placeholder-white bg-transparent border-2 border-white focus:outline-none focus:bg-transparent"
                 type="email"
                 placeholder="Email"
+                id="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
               />
               <input
                 className="w-full px-6 py-3 mt-5 text-sm font-medium text-white placeholder-white bg-transparent border-2 border-white focus:outline-none focus:bg-transparent"
                 type="password"
                 placeholder="Password"
+                id="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
               />
-              <button className="flex items-center justify-center w-full py-4 mt-5 font-semibold tracking-wide text-white transition-all duration-300 ease-in-out bg-transparent border-2 border-white hover:bg-white hover:text-black focus:shadow-outline focus:outline-none">
+              <button
+                className="flex items-center justify-center w-full py-4 mt-5 font-semibold tracking-wide text-white transition-all duration-300 ease-in-out bg-transparent border-2 border-white hover:bg-white hover:text-black focus:shadow-outline focus:outline-none"
+                onClick={onSignUp}
+              >
                 <svg
                   className="w-6 h-6 -ml-2"
                   fill="none"
@@ -38,10 +88,25 @@ export default function SignUpPage() {
                   <circle cx="8.5" cy="7" r="4" />
                   <path d="M20 8v6M23 11h-6" />
                 </svg>
-                <span className="ml-3">Sign Up</span>
+                <span className="ml-3">
+                  {buttonDisabled ? "No SignUp" : "Sign Up"}
+                </span>
               </button>
             </div>
           </div>
+          <div className="flex justify-center mt-6 text-white">
+            <p>
+              Already a user? <Link href="/login">Login Here</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div>
+          <h1>Processing</h1>
         </div>
       </div>
     </>
